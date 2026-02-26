9919ce9b973c402fa26d1205a7756ec98cee5fab
anjadkt <anjad076@gmail.com>
2026-02-26 12:10:14 +0530

    add axios instance & auto refresh

---
+++ src/shared/services/axios.ts
@@ -0,0 +1,27 @@
+import axios from 'axios'
+
+const api = axios.create({
+  baseURL : "http://localhost:5000/api",
+  withCredentials : true
+});
+
+api.interceptors.response.use(
+  (res)=>res,
+  async(error)=>{
+    const originalRequest = error.config ;
+    if(
+      error.response?.status === 401 && !originalRequest.retry
+    ){
+      originalRequest.retry = true ;
+      try{
+        await api.post("/auth/refresh");
+        return api(originalRequest);
+      }catch(refreshError){
+        console.log("Session expired");
+        return Promise.reject(refreshError);
+      }
+    }
+    return Promise.reject(error);
+  })
+
+export default api
