eea504f2ca02ecdde3007cba6dd2cc1117ba536d
Sharath s <144437869+Sharathsree0@users.noreply.github.com>
2026-03-18 12:26:40 +0530

    Delete src/features/courses/activity/activityApi.ts

--- src/features/courses/activity/activityApi.ts
+++
@@ -1,6 +0,0 @@
-import api from "../../../shared/services/axios";
-
-export const getMyActivity = async (status: string) => {
-  const res = await api.get(`/enrollments?status=${status}`);
-  return res.data;
-};
